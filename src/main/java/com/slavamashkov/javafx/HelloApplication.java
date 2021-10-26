e04787b7e1704990e64133ae3ce3d07d3cd8903d
DayDream-21 <vova13158@gmail.com>
2021-10-26 14:44:49 +0300

    First JavaFX app

--- src/main/java/com/slavamashkov/javafx/HelloApplication.java
+++ src/main/java/com/slavamashkov/javafx/HelloApplication.java
@@ -1,23 +1,37 @@
 package com.slavamashkov.javafx;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public static void main(String[] args) {
+        System.out.println("Запуск JavaFХ-приложения.");
+        launch(args);
     }
 
-    public static void main(String[] args) {
-        launch();
+    public void init() {
+        System.out.println("B теле метода init().");
+    }
+
+    @Override
+    public void start(Stage myStage) {
+        System.out.println("B теле метода start().");
+
+        myStage.setTitle("JavaFX Skeleton.");
+
+        FlowPane rootNode = new FlowPane();
+
+        Scene myScene = new Scene(rootNode, 300, 200);
+
+        myStage.setScene(myScene);
+
+        myStage.show () ;
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("B теле метода stop().");
     }
 }
