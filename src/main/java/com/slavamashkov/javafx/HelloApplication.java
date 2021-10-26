c824222a8c312939555098292c5cecf499d1a90a
DayDream-21 <vova13158@gmail.com>
2021-10-26 21:30:35 +0300

    Added label

--- src/main/java/com/slavamashkov/javafx/HelloApplication.java
+++ src/main/java/com/slavamashkov/javafx/HelloApplication.java
@@ -2,36 +2,28 @@ package com.slavamashkov.javafx;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
     public static void main(String[] args) {
-        System.out.println("Запуск JavaFХ-приложения.");
         launch(args);
-    }
-
-    public void init() {
-        System.out.println("B теле метода init().");
     }
 
     @Override
     public void start(Stage myStage) {
-        System.out.println("B теле метода start().");
-
         myStage.setTitle("JavaFX Skeleton.");
 
         FlowPane rootNode = new FlowPane();
+        Label myLabel = new Label("This is a JavaFX label");
+
+        rootNode.getChildren().add(myLabel);
 
         Scene myScene = new Scene(rootNode, 300, 200);
 
         myStage.setScene(myScene);
-
-        myStage.show () ;
-    }
-
-    @Override
-    public void stop() {
-        System.out.println("B теле метода stop().");
+        myStage.show();
     }
 }
