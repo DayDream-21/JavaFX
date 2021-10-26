5e6f4ee6bcda419439673e08408d8367cb0ed4bd
DayDream-21 <vova13158@gmail.com>
2021-10-26 22:06:44 +0300

    Tested buttons

--- src/main/java/com/slavamashkov/javafx/HelloApplication.java
+++ src/main/java/com/slavamashkov/javafx/HelloApplication.java
@@ -1,6 +1,9 @@
 package com.slavamashkov.javafx;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -8,22 +11,31 @@ import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
+    Label response;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage myStage) {
-        myStage.setTitle("JavaFX Skeleton.");
+        myStage.setTitle("Buttons test.");
 
-        FlowPane rootNode = new FlowPane();
-        Label myLabel = new Label("This is a JavaFX label");
-
-        rootNode.getChildren().add(myLabel);
+        FlowPane rootNode = new FlowPane(10, 10);
+        rootNode.setAlignment(Pos.CENTER);
 
         Scene myScene = new Scene(rootNode, 300, 200);
-
         myStage.setScene(myScene);
+
+        response = new Label("Push a button");
+        Button btnA = new Button("A");
+        Button btnB = new Button("B");
+
+        btnA.setOnAction(actionEvent -> response.setText("Pushed A"));
+        btnB.setOnAction(actionEvent -> response.setText("Pushed B"));
+
+        rootNode.getChildren().addAll(btnA, btnB, response);
+
         myStage.show();
     }
 }
