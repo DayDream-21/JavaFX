bf0216c0fc44b82f9e3ded4221a13eeaae2661c7
DayDream-21 <vova13158@gmail.com>
2021-11-04 13:12:14 +0300

    Tested ImageView class

--- src/main/java/com/slavamashkov/javafx/HelloApplication.java
+++ src/main/java/com/slavamashkov/javafx/HelloApplication.java
@@ -1,15 +1,18 @@
 package com.slavamashkov.javafx;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.MalformedURLException;
 
 public class HelloApplication extends Application {
     public static void main(String[] args) {
@@ -17,7 +20,7 @@ public class HelloApplication extends Application {
     }
 
     @Override
-    public void start(Stage myStage) {
+    public void start(Stage myStage) throws MalformedURLException {
         myStage.setTitle("Text field test.");
         Label sumSignLabel = new Label(" + ");
 
@@ -26,6 +29,13 @@ public class HelloApplication extends Application {
 
         Scene myScene = new Scene(rootNode, 300, 200);
         myStage.setScene(myScene);
+
+        File file = new File("D:\\JavaProjects\\JavaFX\\src\\main\\resources\\images\\green_check_mark.png");
+        String localUrl = file.toURI().toURL().toString();
+
+        Image greenCheckMark = new Image(localUrl);
+
+        ImageView greenCheckMarkIV = new ImageView(greenCheckMark);
 
         Button equalsButton = new Button(" = ");
 
@@ -37,9 +47,11 @@ public class HelloApplication extends Application {
         resultTextField.setMaxWidth(80);
 
         equalsButton.setOnAction(actionEvent ->
-                resultTextField.setText(
-                        String.valueOf(Integer.parseInt(aTextField.getText()) + Integer.parseInt(bTextField.getText()))
-                ));
+                {
+                    resultTextField.setText(String.valueOf(Integer.parseInt(aTextField.getText()) + Integer.parseInt(bTextField.getText())));
+                    rootNode.getChildren().add(greenCheckMarkIV);
+                }
+        );
 
         rootNode.getChildren().addAll(aTextField, sumSignLabel, bTextField, equalsButton, resultTextField);
 
